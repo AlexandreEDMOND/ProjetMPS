d50266bda61adaf6b755c175a533fcec5b1bd805
Alex <alexandre.edmond31@gmail.com>
2025-01-07 11:44:54 +0100

    POO finie

--- model_face_recognition.py
+++ model_face_recognition.py
@@ -15,131 +15,127 @@ import shutil
 class ModelFaceRecognition:
 
     def __init__(self, path_raw_image):
+
+        # Path du dossier initiale
         self.path_raw_image = path_raw_image
+        # Path du dossier réduit
+        self.path_reduced_image = "data/reduced"
+        # Path du dossier des visages
+        self.path_face_image = "data/faces"
 
-        last_folder_name = os.path.basename(os.path.normpath(path_raw_image))
+        self.new_width = 800
+        self.new_height = 600
 
-        self.path_reduced_image = os.path.join("data/reduced", last_folder_name)
-        self.path_face_image = os.path.join("data/faces", last_folder_name)
-
-        self.max_width = 800
-        self.max_height = 600
+        self.faces_data = []
     
+    # Fonction pour réduire la taille des images de path_raw_image pour les transformer dans path_reduced_image
+    def reduction_image(self, reduc_coeff=0.1):
 
-    def reduction_image(self):
-
+        # Nettoyage et création du dossier final
         remove_folder_if_exists(self.path_reduced_image)
         
+        # On parcourt tous les sous-dossier du dossier raw_image
+        # Cela correspond à différentes soirées
         for folder_name in os.listdir(self.path_raw_image):
+
             input_folder = os.path.join(self.path_raw_image, folder_name)
             output_folder = os.path.join(self.path_reduced_image, folder_name)
             
-            if os.path.isdir(input_folder):
-                if not os.path.exists(output_folder):
-                    os.makedirs(output_folder)
+            # On créer le sous-dossier
+            os.makedirs(output_folder)
             
-                # Récupère tous les noms de fichiers
-                images = [f for f in os.listdir(input_folder) 
-                            if f.lower().endswith(('.jpg', '.jpeg', '.png'))]
+            # Récupère tous les noms de fichiers du sous-dossier raw
+            images = [f for f in os.listdir(input_folder) 
+                        if f.lower().endswith(('.jpg', '.jpeg', '.png'))]
 
-                for img_name in tqdm(images, desc=f"Réduction de {folder_name}"):
-                    img_path = os.path.join(input_folder, img_name)
-                    img = cv2.imread(img_path)
+            for img_name in tqdm(images, desc=f"Réduction de {folder_name}"):
+                img_path = os.path.join(input_folder, img_name)
 
-                    if img is None:
-                        # Si cv2 ne parvient pas à lire l'image, on skip
-                        print(f"Impossible de lire {img_path}, on saute ce fichier.")
-                        continue
+                # On ouvre l'image
+                img = cv2.imread(img_path)
 
-                    h, w = img.shape[:2]
-                    
-                    # Calcul du ratio de redimensionnement pour garder les proportions
-                    scale_w = self.max_width / w
-                    scale_h = self.max_height / h
-                    scale = min(scale_w, scale_h, 1.0)  # éviter d'agrandir si plus petit que max
-                    
-                    new_w = int(w * scale)
-                    new_h = int(h * scale)
+                shape_img = img.shape
 
-                    # Redimensionnement
-                    resized_img = cv2.resize(img, (new_w, new_h), interpolation=cv2.INTER_AREA)
-                    
-                    # Sauvegarde
-                    out_path = os.path.join(output_folder, img_name)
-                    cv2.imwrite(out_path, resized_img)
+                # Réduction de l'image
+                width = int(shape_img[1] * reduc_coeff)
+                height = int(shape_img[0] * reduc_coeff)
+                dim = (width, height)
 
-                    # Debug
-                    # print(f"[OK] {img_name} redimensionnée et sauvegardée -> {out_path}")
+                # Redimensionnement
+                resized_img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+                
+                # Sauvegarde
+                out_path = os.path.join(output_folder, img_name)
+                cv2.imwrite(out_path, resized_img)
+
+                # Debug
+                # print(f"[OK] {img_name} redimensionnée et sauvegardée -> {out_path}")
     
-    def detect_and_extract_faces(self):
 
-        if not os.path.exists(self.path_face_image):
-            os.makedirs(self.path_face_image)
+
+
+    def detect_and_extract_faces(self, method_face_location="hog", model_face_encoding="large"):
+
+        remove_folder_if_exists(self.path_face_image)
 
         for folder_name in os.listdir(self.path_reduced_image):
             input_folder = os.path.join(self.path_reduced_image, folder_name)
             output_folder = os.path.join(self.path_face_image, folder_name)
             
-            if os.path.isdir(input_folder):
+            os.makedirs(output_folder)  
 
-                if os.path.exists(output_folder):
-                    for file in os.listdir(output_folder):
-                        file_path = os.path.join(output_folder, file)
-                        if os.path.isfile(file_path):
-                            os.remove(file_path)
-                else:
-                    os.makedirs(output_folder)  
+            images = sorted([f for f in os.listdir(input_folder) if f.lower().endswith(('.jpg', '.jpeg', '.png'))])
 
-                images = sorted([f for f in os.listdir(input_folder) if f.lower().endswith(('.jpg', '.jpeg', '.png'))])
+            self.faces_data = []
+            face_global_id = 0  # Compteur pour chaque visage crop
 
-                all_faces_data = []
-                face_global_id = 0  # Compteur pour chaque visage crop
-
-                for img_name in images:
-                    img_path = os.path.join(input_folder, img_name)
+            for img_name in tqdm(images, desc=f"Traitement de {folder_name}"):
+                img_path = os.path.join(input_folder, img_name)
+                
+                # Utilise face_recognition pour lire l'image (ou cv2, au choix)
+                image = face_recognition.load_image_file(img_path)
+                
+                # Localisations des visages
+                face_locations = face_recognition.face_locations(image, model=method_face_location) 
+                # model="hog" est plus rapide que "cnn"
+                
+                # Embeddings de chaque visage
+                face_encodings = face_recognition.face_encodings(image, face_locations, model=model_face_encoding)
+                # model="large" est plus précis que "small"
+                # “small” (default) returns 5 points but is faster.
+                
+                for i, (loc, enc) in enumerate(zip(face_locations, face_encodings)):
+                    top, right, bottom, left = loc
                     
-                    # Utilise face_recognition pour lire l'image (ou cv2, au choix)
-                    image = face_recognition.load_image_file(img_path)
+                    # Crop du visage (en se basant sur face_recognition qui renvoie en (top, right, bottom, left))
+                    face_image = image[top:bottom, left:right]
                     
-                    # Localisations des visages
-                    face_locations = face_recognition.face_locations(image, model="hog") 
-                    # model="hog" est plus rapide que "cnn"
+                    # Convertir en format BGR pour l’enregistrement avec cv2
+                    face_image_bgr = cv2.cvtColor(face_image, cv2.COLOR_RGB2BGR)
                     
-                    # Embeddings de chaque visage
-                    face_encodings = face_recognition.face_encodings(image, face_locations, model="large")
-                    # model="large" est plus précis que "small"
-                    # “small” (default) returns 5 points but is faster.
+                    face_crop_name = f"{os.path.splitext(img_name)[0]}_face{face_global_id}.jpg"
+                    face_crop_path = os.path.join(output_folder, face_crop_name)
                     
-                    for i, (loc, enc) in enumerate(zip(face_locations, face_encodings)):
-                        top, right, bottom, left = loc
-                        
-                        # Crop du visage (en se basant sur face_recognition qui renvoie en (top, right, bottom, left))
-                        face_image = image[top:bottom, left:right]
-                        
-                        # Convertir en format BGR pour l’enregistrement avec cv2
-                        face_image_bgr = cv2.cvtColor(face_image, cv2.COLOR_RGB2BGR)
-                        
-                        face_crop_name = f"{os.path.splitext(img_name)[0]}_face{face_global_id}.jpg"
-                        face_crop_path = os.path.join(output_folder, face_crop_name)
-                        
-                        # Sauvegarde du crop
-                        cv2.imwrite(face_crop_path, face_image_bgr)
-                        
-                        # Sauvegarder infos dans la liste
-                        all_faces_data.append({
-                            "face_id": face_global_id,
-                            "embedding": enc,
-                            "photo_name": img_name,
-                            "crop_path": face_crop_path
-                        })
-                        
-                        face_global_id += 1
+                    # Sauvegarde du crop
+                    cv2.imwrite(face_crop_path, face_image_bgr)
                     
-                    print(f"[OK] {len(face_encodings)} visages détectés dans {img_name}.")
+                    # Sauvegarder infos dans la liste
+                    self.faces_data.append({
+                        "face_id": face_global_id,
+                        "embedding": enc,
+                        "photo_name": img_name,
+                        "crop_path": face_crop_path
+                    })
+                    
+                    face_global_id += 1
+                
+                # Débug
+                # print(f"[OK] {len(face_encodings)} visages détectés dans {img_name}.")
     
-    def cluster_faces(self, all_faces_data):
+
+    def cluster_faces(self):
         # On récupère tous les embeddings dans un array numpy
-        embeddings = np.array([face["embedding"] for face in all_faces_data])
+        embeddings = np.array([face["embedding"] for face in self.faces_data])
 
         # Appliquer DBSCAN
         dbscan = DBSCAN(metric='euclidean', eps=0.5, min_samples=2)
@@ -147,25 +143,26 @@ class ModelFaceRecognition:
 
         labels = dbscan.labels_  # cluster_id pour chaque embedding (ou -1 si outlier)
 
-        for i, face in enumerate(all_faces_data):
+        # On rajoute le cluster dans les données de faces_data
+        for i, face in enumerate(self.faces_data):
             face["cluster_id"] = labels[i]
 
         print(f"Nombre de clusters trouvés (hors -1) : {len(set(labels) - {-1})}")
 
 
-    def visualize_clusters(self, all_faces_data):
+    def visualize_clusters(self, nb_affichage=5):
         
         # Regrouper les visages par cluster_id
         clusters_dict = defaultdict(list)
-        for face in all_faces_data:
+        for face in self.faces_data:
             cluster_id = face["cluster_id"]
             clusters_dict[cluster_id].append(face["crop_path"])
 
         # Ignorer le cluster -1 (outliers), si souhaité
         cluster_ids = sorted([cid for cid in clusters_dict.keys() if cid != -1])
 
-        print(f"Clusters ignorés (outliers) : {clusters_dict.get(-1, [])}")
-        print(f"Clusters valides : {cluster_ids}")
+        print(f"Clusters ignorés (outliers) : {len(clusters_dict.get(-1, []))}")
+        print(f"Clusters valides : {len(cluster_ids)}")
 
         for cid in cluster_ids:
             face_paths = clusters_dict[cid]
@@ -175,7 +172,7 @@ class ModelFaceRecognition:
             plt.suptitle(f"Cluster {cid}\n{len(face_paths)} visages", fontsize=14)
             
             # Afficher un échantillon de visages (jusqu’à max_faces_per_cluster)
-            sample_paths = face_paths[:5]
+            sample_paths = face_paths[:nb_affichage]
             
             for i, path in enumerate(sample_paths):
                 img_bgr = cv2.imread(path)
